4e4da0172320ee74168576471cbcdc69912ef325
HoangHai28 <hoanghai28702@gmail.com>
2024-07-31 09:26:25 +0700

    update js

--- js/site.js
+++ js/site.js
@@ -1,3 +1,23 @@
 (function (html) {
     html.className = html.className.replace(/\bno-js\b/, "js");
 })(document.documentElement);
+
+    var wpcf7 = {
+      apiSettings: {
+        root: "https:\/\/phatnguoi.org\/wp-json\/contact-form-7\/v1",
+        namespace: "contact-form-7\/v1",
+      },
+      cached: "1",
+    };
+  
+    var flatsomeVars = {
+        ajaxurl: "https:\/\/phatnguoi.org\/wp-admin\/admin-ajax.php",
+        rtl: "",
+        sticky_height: "70",
+        lightbox: {
+          close_markup:
+            '<button title="%title%" type="button" class="mfp-close"><svg xmlns="http:\/\/www.w3.org\/2000\/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"><\/line><line x1="6" y1="6" x2="18" y2="18"><\/line><\/svg><\/button>',
+          close_btn_inside: false,
+        },
+        user: { can_edit_pages: false },
+      };
